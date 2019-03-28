354fd68ce93e58ac579f6e8ebb1c43035d565149
daaquan <daaquan@gmail.com>
2019-03-28 16:17:12 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# phpfan
+# phpfan.net
+
---
+++ app/Model/Events.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+class Events extends \Eloquent
+{
+    protected $table = 'events';
+    protected $softDelete = true;
+    public function users()
+    {
+        return $this->belongsToMany('User', 'event_user')->withPivot('user_id');
+    }
+}
